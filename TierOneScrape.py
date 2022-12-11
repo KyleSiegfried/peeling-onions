196539bd18feb853fff04d25e214a709b96d22f9
KyleSiegfried <ballparks4him44@gmail.com>
2022-12-11 02:45:37 -0500

    Made ask for company first

--- TierOneScrape.py
+++ TierOneScrape.py
@@ -3,6 +3,9 @@ import random
 import re
 import requests
 
+#Company we are scraping for
+company = str(input("What company do you want to scrape for? "))
+
 #List of spoofed user agents to prevent site from blocking our traffic
 ua_list = ["Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19577"
     ,"Mozilla/5.0 (X11) AppleWebKit/62.41 (KHTML, like Gecko) Edge/17.10859 Safari/452.6", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2656.18 Safari/537.36"
@@ -76,5 +79,5 @@ def scraper(company):
         raw_text = dark_content.get_text()
         checked_onions.append(url)
         return company in raw_text
-scraper(str(input("What company do you want to scrape for? ")))
+scraper(company)
     
